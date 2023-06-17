4dbf76f2d2be7357f17932095cafbc298bd0c3e4
ydvmudit07 <ydvmudit07@gmail.com>
2023-06-17 10:41:41 +0530

    testing

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -1,9 +1,24 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappcord", function () {
-
+  describe("Deployment", function(){
+    it("Sets the name & symbol", async () => {
+      //Deploy contract
+      const Dappcord = await ethers.getContractFactory("Dappcord")
+      let dappcord = await Dappcord.deploy("Dappcord", "DC")
+      //Fetch name
+      let result = await dappcord.name()
+      //Check name
+      expect(result).to.equal("Dappcord")
+      //Fetch symbol
+      result = await dappcord.symbol()
+      //Check symbol
+      expect(result).to.equal("DC")
+    })
+  })
 })
