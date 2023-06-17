5b660f60fe49ad074811ab450a114225b62471b8
ydvmudit07 <ydvmudit07@gmail.com>
2023-06-18 00:34:25 +0530

    mint

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -5,10 +5,43 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Dappcord is ERC721 {
 
-    constructor(string memory _name, string memory _symbol) 
+    address public owner;
+    uint256 public totalSupply;
+    uint256 public totalChannel;
+
+    struct Channel {
+        uint256 id;
+        string name;
+        uint256 cost;
+    }
+
+    mapping(uint256 => Channel) public channels;
+    mapping(uint256 => mapping(address => bool)) public hasJoined;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor (string memory _name, string memory _symbol) 
         ERC721(_name, _symbol) 
     {
+        owner = msg.sender;
+    }
 
+    function createChannel(string memory _name, uint256 _cost) public onlyOwner  {
+        require(msg.sender == owner);
+        totalChannel++;
+        channels[totalChannel] = Channel(totalChannel, _name, _cost);
+    }
+
+    function mint(uint256 _id) public payable {
+        hasJoined[_id][msg.sender] = true;
+        totalSupply++;
+        _safeMint(msg.sender, totalSupply);
+    }
+    function getChannel(uint256 _id) public view returns(Channel memory) {
+        return channels[_id];
     }
 
 }
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -6,19 +6,84 @@ const tokens = (n) => {
 }
 
 describe("Dappcord", function () {
+  let dappcord
+  let deployer, user
+  const NAME = 'Dappcord'
+  const SYMBOL = 'DC'
+  beforeEach(async () => {
+    //Setup accounts
+    [deployer, user] = await ethers.getSigners()
+
+    //Deploy contract
+    const Dappcord = await ethers.getContractFactory("Dappcord")
+    dappcord = await Dappcord.deploy(NAME, SYMBOL)
+    
+    //Create a channel
+    const transaction = await dappcord.connect(deployer).createChannel("general", tokens(1))
+    await transaction.wait()
+  })
+  
+  
   describe("Deployment", function(){
-    it("Sets the name & symbol", async () => {
-      //Deploy contract
-      const Dappcord = await ethers.getContractFactory("Dappcord")
-      let dappcord = await Dappcord.deploy("Dappcord", "DC")
+    it("Sets the name", async () => {
       //Fetch name
       let result = await dappcord.name()
       //Check name
       expect(result).to.equal("Dappcord")
+    })
+
+    it("Sets the symbol", async () => {
       //Fetch symbol
-      result = await dappcord.symbol()
+      let result = await dappcord.symbol()
       //Check symbol
       expect(result).to.equal("DC")
     })
+
+    it("Sets the owner", async () => {
+      let result = await dappcord.owner()
+      expect(result).to.equal(deployer.address)
+    })
+
+  })
+  
+  describe("Creating Channels", () => {
+    it('Returns total channels', async() => {
+      const result = await dappcord.totalChannel()
+      expect(result).to.be.equal(1)
+    })
+    
+    it('Returns channels attribute', async() => {
+      const channel = await dappcord.getChannel(1)
+      expect(channel.id).to.be.equal(1)
+      expect(channel.name).to.be.equal("general")
+      expect(channel.cost).to.be.equal(tokens(1))
+    })
+  })
+  
+  describe("Joining Channels", () => {
+    const ID = 1;
+    const AMOUNT = ethers.utils.parseUnits("1", 'ether')
+    
+    beforeEach(async () => {
+      const transaction = await dappcord.connect(user).mint(ID, { value: AMOUNT})
+      await transaction.wait()
+    })
+    
+    it('Joins the user', async () => {
+      const result = await dappcord.hasJoined(ID, user.address)
+      expect(result).that.be.equal(true)
+    })
+    
+    it('Increases total supply', async () => {
+      const result = await dappcord.totalSupply()
+      expect(result).to.be.equal(ID)
+    })
+    
+    // it('Updates the contract balance', async () => {
+    //   const result = await dappcord.getBalance(dappcord.address)
+    //   expect(result).to.be.equal(AMOUNT)
+    // })
   })
 })
+  
+  
